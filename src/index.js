ac2260a0c5018bf99b1212b56f8c00f0601f9bd9
Павлов Дмитрий <pavlovdo@it2g.ru>
2024-08-12 20:23:53 +0700

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let currentStr='';
+
+  for (let i = 0; i < str.length; i++) {
+    const currentElement = str[i];
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        let startBracket=bracketsConfig[j][0];
+        let endBracket=bracketsConfig[j][1];
+
+        if (currentElement == endBracket)
+        {
+          if (currentStr.length==0 && startBracket!= endBracket)
+            return false;
+
+          if (currentStr.slice(-1)==startBracket)
+          {
+            currentStr=currentStr.substring(0, currentStr.length-1);
+            break;
+          }
+          if (startBracket!= endBracket)
+            return false;
+        }
+
+        if (currentElement == startBracket)
+        {
+          currentStr=currentStr+currentElement;
+          break;
+        }
+    }
+  }
+
+  if (currentStr.length>0)
+    return false;
+
+  return true;
 }
